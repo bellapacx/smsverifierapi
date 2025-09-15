1ced2e9f27468aaed4e335d94adde7fc0d729299
bellapacx <bellapacx@gmail.com>
2025-09-16 00:52:19 +0300

    Ignore Firebase service accountddddddddsdffdddddddddlddssdd

--- utils/telebirr_received_parser.go
+++ utils/telebirr_received_parser.go
@@ -13,29 +13,22 @@ type TelebirrReceivedParser struct{}
 func (p *TelebirrReceivedParser) Parse(sms string) (*ParsedSMS, error) {
 	parsed := &ParsedSMS{}
 
-	// Transaction ID (case-insensitive)
-	txRe := regexp.MustCompile(`(?i)transaction number (\w+)`)
+	// Transaction ID
+	txRe := regexp.MustCompile(`(?i)transaction number is (\w+)`)
 	if m := txRe.FindStringSubmatch(sms); len(m) > 1 {
 		parsed.TransactionID = m[1]
 	} else {
 		return nil, errors.New("Transaction ID not found")
 	}
 
-	// Account (case-insensitive)
-	accRe := regexp.MustCompile(`(?i)telebirr Account (\d+)`)
-	if m := accRe.FindStringSubmatch(sms); len(m) > 1 {
-		parsed.Account = m[1]
-	} else {
-		return nil, errors.New("Account not found")
-	}
-
-	// Sender Name (relaxed)
-	senderRe := regexp.MustCompile(`(?i)from ([A-Za-z ]+?) to your`)
-	if m := senderRe.FindStringSubmatch(sms); len(m) > 1 {
+	// Sender (Name + Phone inside parentheses)
+	senderRe := regexp.MustCompile(`(?i)from ([A-Za-z ]+)\((\d+)\)`)
+	if m := senderRe.FindStringSubmatch(sms); len(m) > 2 {
 		parsed.SenderName = strings.TrimSpace(m[1])
+		parsed.Account = m[2] // phone number in parentheses
 	}
 
-	// Amount (case-insensitive)
+	// Amount received
 	amountRe := regexp.MustCompile(`(?i)received ETB ([\d,]+\.\d+)`)
 	if m := amountRe.FindStringSubmatch(sms); len(m) > 1 {
 		amt, _ := strconv.ParseFloat(strings.ReplaceAll(m[1], ",", ""), 64)
@@ -44,15 +37,17 @@ func (p *TelebirrReceivedParser) Parse(sms string) (*ParsedSMS, error) {
 		return nil, errors.New("Amount not found")
 	}
 
-	// Date (yyyy-mm-dd hh:mm:ss)
-	dateRe := regexp.MustCompile(`(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})`)
+	// Date (dd/mm/yyyy hh:mm:ss)
+	dateRe := regexp.MustCompile(`(\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2})`)
 	if m := dateRe.FindStringSubmatch(sms); len(m) > 1 {
-		dt, _ := time.Parse("2006-01-02 15:04:05", m[1])
-		parsed.Date = dt
+		dt, err := time.Parse("02/01/2006 15:04:05", m[1])
+		if err == nil {
+			parsed.Date = dt
+		}
 	}
 
-	// Balance (case-insensitive)
-	balRe := regexp.MustCompile(`(?i)current balance is ETB ([\d,]+\.\d+)`)
+	// Balance
+	balRe := regexp.MustCompile(`(?i)Account balance is ETB ([\d,]+\.\d+)`)
 	if m := balRe.FindStringSubmatch(sms); len(m) > 1 {
 		bal, _ := strconv.ParseFloat(strings.ReplaceAll(m[1], ",", ""), 64)
 		parsed.Balance = bal
