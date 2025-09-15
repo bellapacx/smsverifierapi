738d63ead6e0c950d95c5183efdafa5d3b9e3a56
bellapacx <bellapacx@gmail.com>
2025-09-15 20:06:08 +0300

    Ignore Firebase service accountdddddd

--- utils/parser.go
+++ utils/parser.go
@@ -18,6 +18,7 @@ type ParsedSMS struct {
 }
 
 func ParseBankSMS(sms string) (*ParsedSMS, error) {
+	// Regex: capture account, amount, sender, date, transaction ID, balance
 	re := regexp.MustCompile(`Account (\d+\*+\d+) has been Credited with ETB ([\d,\.]+) from (.*?), on (\d{2}/\d{2}/\d{4} \d{2}:\d{2}:\d{2}) with Ref No (\w+) Your Current Balance is ETB ([\d,\.]+)`)
 
 	matches := re.FindStringSubmatch(sms)
@@ -26,9 +27,23 @@ func ParseBankSMS(sms string) (*ParsedSMS, error) {
 		return nil, nil
 	}
 
-	amount, _ := strconv.ParseFloat(strings.ReplaceAll(matches[2], ",", ""), 64)
-	date, _ := time.Parse("02/01/2006 15:04:05", matches[4])
-	balance, _ := strconv.ParseFloat(strings.ReplaceAll(matches[6], ",", ""), 64)
+	amount, err := strconv.ParseFloat(strings.ReplaceAll(matches[2], ",", ""), 64)
+	if err != nil {
+		log.Println("Failed to parse amount:", matches[2])
+		return nil, err
+	}
+
+	date, err := time.Parse("02/01/2006 15:04:05", matches[4])
+	if err != nil {
+		log.Println("Failed to parse date:", matches[4])
+		return nil, err
+	}
+
+	balance, err := strconv.ParseFloat(strings.ReplaceAll(matches[6], ",", ""), 64)
+	if err != nil {
+		log.Println("Failed to parse balance:", matches[6])
+		return nil, err
+	}
 
 	return &ParsedSMS{
 		Account:       matches[1],
